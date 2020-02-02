df4ad86e2dd51269ab387994d7883c6799307816
electronstudio <mercurial@fantastic.me.uk>
2020-02-02 23:07:06 +0000

    removed stuff

--- enemy.gd
+++ enemy.gd
@@ -7,29 +7,29 @@ export var FIRE_RATE = 0.01
 var Bullet = preload("res://enemy_bullet.tscn")
 onready var player = get_node("/root/main/player")
 
-func _ready():
-	position = player.position + Vector2.RIGHT.rotated(rand_range(0, PI*2)) * 5000
-	rotation = player.position.angle_to_point(position) 
+#func _ready():
+#	position = player.position + Vector2.RIGHT.rotated(rand_range(0, PI*2)) * 5000
+#	rotation = player.position.angle_to_point(position) 
 
 func _process(delta):
 	var d = player.position.angle_to_point(position) 
 	rotation = Util.rotate_toward(rotation, d, TURNING*delta)
 	position += Vector2.RIGHT.rotated(rotation) * VELOCITY * delta
-	
+
 	if position.distance_to(player.position) > 7000:
 		queue_free()
-	
+
 	if randf()<FIRE_RATE:
 		Bullet.instance().init(self, 3000)
-	
-func _on_enemy_area_entered(area):
-	$explosion.play()
-	$AnimationPlayer.play("fade")
-	$CollisionPolygon2D.queue_free()
-	$CPUParticles2D.emitting = true
-	$CPUParticles2D.show()
-	player.score += 1
-	get_node("/root/main/HUD/score").text = str(player.score)
-	yield(get_tree().create_timer(1.0), "timeout")
-	queue_free()
+#
+#func _on_enemy_area_entered(area):
+#	$explosion.play()
+#	$AnimationPlayer.play("fade")
+#	$CollisionPolygon2D.queue_free()
+#	$CPUParticles2D.emitting = true
+#	$CPUParticles2D.show()
+#	player.score += 1
+#	get_node("/root/main/HUD/score").text = str(player.score)
+#	yield(get_tree().create_timer(1.0), "timeout")
+#	queue_free()
 	
--- player.gd
+++ player.gd
@@ -18,38 +18,6 @@ func _process(delta):
 	gamepad(delta)
 	position += Vector2.RIGHT.rotated(rotation) * velocity * delta
 	
-func _on_player_area_entered(area):
-	health -= 1
-	get_node("../HUD/health").value = health
-	if health <= 0:
-		get_tree().reload_current_scene()
-	hitFX()
-
-func hitFX():
-	$crash_sound.play()
-	modulate = Color(1000, 0, 0, 255)
-	yield(get_tree().create_timer(1.0), "timeout")
-	modulate = Color(1, 1, 1, 255)
-	
-var virtual_stick_direction = Vector2.ZERO
-
 func gamepad(delta):
-	var input = Vector2(Input.get_joy_axis(0, 0), Input.get_joy_axis(0, 1)) + virtual_stick_direction
-	if input.length() > 0.2:
-		var direction = input.angle()
-		rotation = Util.rotate_toward(rotation, direction, turning * delta) 
+	pass
 		
-var virtual_stick_origin = Vector2.ZERO
-
-func _input(event):
-	if event is InputEventScreenTouch:
-		if event.position.x < get_viewport().size.x/2.0:
-			if event.pressed:
-				virtual_stick_origin = event.position
-		else:
-			if event.pressed:
-				Input.action_press("fire")
-			else:
-				Input.action_release("fire")
-	elif event is InputEventScreenDrag and event.position.x < get_viewport().size.x/2.0:
-		virtual_stick_direction =  (event.position - virtual_stick_origin).normalized()
