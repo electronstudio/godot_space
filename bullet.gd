52a714bc3acb1dd49a20e33bf84ed4b41b55c889
electronstudio <misc@fantastic.me.uk>
2020-02-05 14:36:25 +0000

    remove bullet code

--- bullet.gd
+++ bullet.gd
@@ -7,16 +7,9 @@ func _ready():
 	$sound.play()
 	position += Vector2.RIGHT.rotated(rotation) * 150
 
-func _process(delta):
-	position += Vector2.RIGHT.rotated(rotation) * velocity * delta
-	if position.distance_to(player.position) > 5000:
-		queue_free()
-
-func _on_bullet_area_entered(area):
-	queue_free()
-
 func init(parent, vel):
 		position = parent.position
 		rotation = parent.rotation
 		velocity = vel
 		parent.get_node("/root/main/bullets").add_child(self)
+		
